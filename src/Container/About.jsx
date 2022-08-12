c7bef86ce9b05288f92aea8bdf74fb33ce247ece
ikaushiksharma <kaushik445566@gmail.com>
2022-08-12 16:08:45 +0530

    Mobile Version Updated

--- src/Container/About.jsx
+++ src/Container/About.jsx
@@ -22,7 +22,57 @@ const About = () => {
           controls={false}
           className="object-cover w-full h-full"
         />
-        <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center gap-24 comforto bg-Overlay">
+
+        {/* MOBILE VERSION  */}
+
+        <div className="lg:hidden absolute top-0 right-0 left-0 bottom-0 items-center bg-Overlay text-white comforto flex flex-col justify-center">
+          <Link
+            to="/"
+            className="absolute duration-700 text-white top-10 text-3xl left-10 hover:text-5xl"
+          >
+            <IoHomeOutline />
+          </Link>
+          <p className="text-white drop-shadow-xl text-5xl duration-700 ease-in-out hover:-translate-y-9">
+            Kaushik <br /> Sharma
+          </p>
+          <div className="w-60 h-1 my-10 bg-gray-300  rounded-lg "></div>
+          <p className="text-gray-300 drop-shadow-lg text-4xl text-center duration-700 ease-in-out hover:-translate-y-9">
+            Developer <br /> Coder <br /> Learner
+          </p>
+          <div className=" absolute top-3/4 right-0 left-0 flex flex-row justify-center gap-10 text-white mt-3 text-2xl">
+            <a
+              href="https://www.github.com/ikaushiksharma"
+              className="hover:text-3xl duration-500"
+            >
+              <AiFillGithub />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/ikaushiksharma"
+              className="hover:text-3xl duration-500"
+            >
+              <AiFillLinkedin />
+            </a>
+            <a
+              href="https://www.instagram.com/ikaushiksharma"
+              className="hover:text-3xl duration-500"
+            >
+              <AiFillInstagram />
+            </a>
+            <a
+              href="mailto:kaushik445566@gmail.com"
+              className="hover:text-3xl duration-500"
+            >
+              <AiFillMail />
+            </a>
+          </div>
+          <div className="absolute bottom-10 hover:tracking-widest duration-700 ease-out text-gray-400">
+            Copyright &copy; 2022 | Kaushik Sharma
+          </div>
+        </div>
+
+        {/* DeskTop Version  */}
+
+        <div className="hidden absolute top-0 bottom-0 left-0 right-0 lg:flex items-center justify-center gap-24 comforto bg-Overlay">
           <Link
             to="/"
             className="absolute duration-700 text-white top-20 text-3xl left-20 hover:text-5xl"
